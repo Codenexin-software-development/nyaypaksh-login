e7bb733753a514f568d3eecdc958f5c5c8fe4096
Mogilipranathi <mogilipranathi1212@gmail.com>
2026-01-30 23:39:17 +0530

    Remove duplicate imports and clean up App.js

--- nyay-paksh/src/App.js
+++ nyay-paksh/src/App.js
@@ -1,38 +1,20 @@
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-
 import LoginPage from "./components/LoginPage";
 import ProfilePage from "./components/ProfilePage";
-
 import "./App.css";
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
-import ProfilePage from "./components/ProfilePage";
-import './App.css';
-
 
 function App() {
   return (
     <Router>
-
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
-
       <div className="App">
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<LoginPage />} />
           
           <Route path="/profile" element={<ProfilePage />} />
-
         </Routes>
       </div>
-
     </Router>
   );
 }
