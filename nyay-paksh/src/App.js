a7dfd3b267f71a7bbdc274b832c9fb9b20a496b7
Mogilipranathi <mogilipranathi1212@gmail.com>
2026-01-30 21:39:35 +0530

    Refactor routes in App.js for ProfilePage

--- nyay-paksh/src/App.js
+++ nyay-paksh/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
-import HomePage from './components/HomePage';
-import './App.css';
-import ProfileSection from "./profile/ProfileSection";
-import ProfilePage from "./profile/ProfilePage";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
+import LoginPage from "./components/LoginPage";
+import ProfilePage from "./components/ProfilePage";
 
-
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile" element={<ProfileSection />} />
-          <Route path="/profile" element={<ProfilePage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </Router>
   );
 }
